95821a0fa58750c37a7602a702c0b849c4b62008
imDatt777 <ashutoshdatt777@gmail.com>
2022-06-26 13:38:39 +0530

    Enhancements

--- src/App.js
+++ src/App.js
@@ -1,52 +1,112 @@
-import "./App.css";
+import React from "react";
+import { useState } from "react";
 import NavBar from "./components/NavBar";
 import { News } from "./components/News";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 function App() {
+  const [progress, setProgress] = useState(0);
   return (
     <>
       <Router>
         <div className="App">
+          <LoadingBar
+            color="#e3714d"
+            progress={progress}
+            onLoaderFinished={() => setProgress(0)}
+          />
           <NavBar />
           <Routes>
             <Route
               exact
               path="/"
-              element={<News key="home" newsCount={9} country="in" category="" />}
+              element={
+                <News
+                  setProgress={setProgress}
+                  key="home"
+                  newsCount={9}
+                  country="in"
+                  category=""
+                />
+              }
             />
             <Route
               exact
               path="/business"
-              element={<News key="business" newsCount={9} country="in" category="business" />}
+              element={
+                <News
+                  setProgress={setProgress}
+                  key="business"
+                  newsCount={9}
+                  country="in"
+                  category="business"
+                />
+              }
             />
             <Route
               exact
               path="/entertainment"
               element={
-                <News key="entertainment" newsCount={9} country="in" category="entertainment" />
+                <News
+                  setProgress={setProgress}
+                  key="entertainment"
+                  newsCount={9}
+                  country="in"
+                  category="entertainment"
+                />
               }
             />
             <Route
               exact
               path="/health"
-              element={<News key="health" newsCount={9} country="in" category="health" />}
+              element={
+                <News
+                  setProgress={setProgress}
+                  key="health"
+                  newsCount={9}
+                  country="in"
+                  category="health"
+                />
+              }
             />
             <Route
               exact
               path="/sports"
-              element={<News key="business" newsCount={9} country="in" category="sports" />}
+              element={
+                <News
+                  setProgress={setProgress}
+                  key="business"
+                  newsCount={9}
+                  country="in"
+                  category="sports"
+                />
+              }
             />
             <Route
               exact
               path="/science"
-              element={<News key="science" newsCount={9} country="in" category="science" />}
+              element={
+                <News
+                  setProgress={setProgress}
+                  key="science"
+                  newsCount={9}
+                  country="in"
+                  category="science"
+                />
+              }
             />
             <Route
               exact
               path="/technology"
               element={
-                <News key="technology" newsCount={9} country="in" category="technology" />
+                <News
+                  setProgress={setProgress}
+                  key="technology"
+                  newsCount={9}
+                  country="in"
+                  category="technology"
+                />
               }
             />
           </Routes>
--- src/components/News.js
+++ src/components/News.js
@@ -2,48 +2,45 @@ import React, { useEffect, useState } from "react";
 import { NewsItem } from "./NewsItem";
 import "../styles/news.scss";
 import Loader from "./Loader";
-
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export const News = (props) => {
-  const { newsCount, country, category } = props;
+  const { newsCount, country, category, setProgress } = props;
   const [articles, setArticles] = useState([]);
   const [page, setPage] = useState(1);
   const [totalArticles, setTotalArticles] = useState(null);
+  // eslint-disable-next-line
   const [isLoading, setIsLoading] = useState(true);
-  let newsUrl = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${newsCount}&page=${page}`;
-
-  const prevClickHandler = async () => {
-    let currentPage = page;
-    if (currentPage > 1) {
-      currentPage--;
-    }
-    setPage(currentPage);
-    newsUrl = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${newsCount}&page=${page}`;
-  };
-
-  const nextClickHandler = async () => {
-    let currentPage = page;
-    currentPage++;
-    setPage(currentPage);
-    newsUrl = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${newsCount}&page=${page}`;
-  };
 
   const getNews = async () => {
+    setProgress(0);
+    const newsUrl = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${newsCount}&page=${page}`;
     const res = await fetch(newsUrl);
+    setProgress(20);
     const data = await res.json();
+    setProgress(40);
     setArticles(data.articles);
-    console.log(data.articles);
+    setProgress(60);
     setTotalArticles(data.totalResults);
+    setProgress(80);
     setIsLoading(false);
+    setProgress(100);
   };
-
-  console.log(articles);
 
   //  UseEffect would run getNews each time url changes
   useEffect(() => {
     getNews();
     // eslint-disable-next-line
-  }, [newsUrl]);
+  }, []);
+
+  const fetchMoreArticles = async () => {
+    const newsUrl = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_API_KEY}&pageSize=${newsCount}&page=${page}`;
+    setPage(page + 1);
+    let res = await fetch(newsUrl);
+    let data = await res.json();
+    setArticles(articles.concat(data.articles));
+    setTotalArticles(data.totalArticles);
+  };
 
   return (
     <div className="home-page-container">
@@ -55,37 +52,29 @@ export const News = (props) => {
       ) : (
         <h1>Latest Stories</h1>
       )}
-      {isLoading && <Loader />}
-      <div className="news-items-container">
-        {articles.map((article, idx) => (
-          <NewsItem
-            key={idx}
-            title={article.title ? article.title.slice(0, 60) : ""}
-            desc={article.description ? article.description.slice(0, 200) : ""}
-            imgUrl={article.urlToImage}
-            newsUrl={article.url}
-            date={article.publishedAt}
-            author={article.author}
-            src={article.source.name}
-          />
-        ))}
-      </div>
-      <div className="button-container">
-        <button
-          disabled={page <= 1}
-          className="prev"
-          onClick={prevClickHandler}
-        >
-          &larr; Previous
-        </button>
-        <button
-          disabled={page + 1 > Math.ceil(totalArticles / newsCount)}
-          className="next"
-          onClick={nextClickHandler}
-        >
-          Next &rarr;
-        </button>
-      </div>
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreArticles}
+        hasMore={articles.length !== totalArticles}
+        loader={<Loader />}
+      >
+        <div className="news-items-container">
+          {articles.map((article, idx) => (
+            <NewsItem
+              key={idx}
+              title={article.title ? article.title.slice(0, 60) : ""}
+              desc={
+                article.description ? article.description.slice(0, 200) : ""
+              }
+              imgUrl={article.urlToImage}
+              newsUrl={article.url}
+              date={article.publishedAt}
+              author={article.author}
+              src={article.source.name}
+            />
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
